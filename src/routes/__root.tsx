4ff82fb47fa4f1d60241eba0149feef7d163aa08
Agam Singh (Call Me Menifester) <120456245+DreamFaang78@users.noreply.github.com>
2026-09-10 18:12:59 +0530

    Update Meta Pixel ID to 2079007509396397

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -122,7 +122,7 @@ function RootShell({ children }: { children: ReactNode }) {
               t.src=v;s=b.getElementsByTagName(e)[0];
               s.parentNode.insertBefore(t,s)}(window, document,'script',
               'https://connect.facebook.net/en_US/fbevents.js');
-              fbq('init', '1354761316257645');
+              fbq('init', '2079007509396397');
               fbq('track', 'PageView');
             `,
           }}
@@ -132,7 +132,7 @@ function RootShell({ children }: { children: ReactNode }) {
             height="1"
             width="1"
             style={{ display: "none" }}
-            src="https://www.facebook.com/tr?id=1354761316257645&ev=PageView&noscript=1"
+            src="https://www.facebook.com/tr?id=2079007509396397&ev=PageView&noscript=1"
           />
         </noscript>
         <script
